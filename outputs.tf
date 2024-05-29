3d894e719ba0f5bfed42e88cfdb98420cbabbbe6
dkool <dkool@dkools-MacBook-Pro.local>
2024-05-29 12:05:59 +0200

    fixing formatting

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "acr" {
   description = "contains container registry related configuration"
-  value = azurerm_container_registry.acr
+  value       = azurerm_container_registry.acr
 }
 
 output "subscriptionId" {
   description = "contains the current subscription id"
-  value = data.azurerm_subscription.current.subscription_id
+  value       = data.azurerm_subscription.current.subscription_id
 }
 
 output "agentpools" {
   description = "contains the agent pools"
-  value = azurerm_container_registry_agent_pool.pools
+  value       = azurerm_container_registry_agent_pool.pools
 }
