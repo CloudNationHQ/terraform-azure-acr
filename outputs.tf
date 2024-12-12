c582f2ca527852e6b6f3fa9b9add18891755e55c
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2024-12-12 09:12:46 +0100

    feat!: small refactor (#77)
    BREAKING CHANGE: some keys regarding registry tokens and secrets have changed, which will cause a replacement.
    
    Co-authored-by: dkool <dkool@dkools-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,6 @@ output "registry" {
   value       = azurerm_container_registry.acr
 }
 
-output "subscription_id" {
-  description = "contains the current subscription id"
-  value       = data.azurerm_subscription.current.subscription_id
-}
-
 output "agentpools" {
   description = "contains the agent pools"
   value       = azurerm_container_registry_agent_pool.pools
